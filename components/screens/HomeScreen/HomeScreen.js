aabb4e7fb814feba77932e7e2862efebf8e4a6ca
Caleb <35348015+CalebMcOlin@users.noreply.github.com>
2021-10-16 15:08:22 -0700

    Setup main menu (#25)
    * created base main menu / cleaned files
    
    * cleaned files
    
    * Created base main menu
    
    * Created HomeScreen component
    
    * Added return statement
    
    * fix typo / renamed functional component

---
+++ components/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Button, View, Text, Alert } from 'react-native';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Home View
+        </Text>
+      </View>
+
+      <Separator />
+
+      <View>
+        <Text style={styles.title}>
+          Live interactive section of the screen. Rotates between "Join" and "Daily Featured Rides"
+        </Text>
+      </View>
+
+      <Separator />
+
+      <View style={styles.fixToText}>
+        <Button
+          title="Calendar"
+          onPress={() => Alert.alert('Opening the Calendar')}
+        />
+        <Button
+          title="Blog"
+          onPress={() => Alert.alert('Opening the Blog')}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default HomeScreen;
