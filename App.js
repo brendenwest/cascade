34e3b2eda190ccb17a58100f1962c6365591d638
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-10-23 23:00:30 -0700

    Ignore the Reanimated 2 warning

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DefaultDrawer } from './components/routes/Drawers';
 
+// Ignore the Reanimated 2 warning
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Reanimated 2']);
+
 const App = () => {
   return (
     <SafeAreaProvider>
