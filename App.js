d8db8b745fe0d8adbabd0fa301092f71a03a2bd9
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-10-23 19:25:31 -0700

    added stack, drawer, and tab navigation

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { HomeStack } from './components/routes/Stacks';
+import { DefaultDrawer } from './components/routes/Drawer';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <HomeStack />
+      <NavigationContainer>
+        <DefaultDrawer />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
