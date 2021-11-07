2c4add68740261fbdd704a90c8e95f020a5c6946
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-11-06 17:58:02 -0700

    implement FlatList for Daily Rides

--- components/screens/DailyRidesScreen/DailyRidesScreen.js
+++ components/screens/DailyRidesScreen/DailyRidesScreen.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import { Styles } from './DailyRidesStyleSheet';
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+import Card from  '../../shared/card';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Ride',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Ride',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Ride',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={Styles.item}>
+    <Text style={Styles.title}>{title}</Text>
+  </View>
+);
 
 const DailyRidesScreen = ({ navigation }) => {
-  return (
-    <View style={Styles.container}>
-      <View>
-        <Text style={Styles.title}>
-          Featured Daily Rides
-        </Text>
-      </View>
+    const renderItem = ({ item }) => (
+      <Item title={item.title} />
+    );  
+    return (
+      <SafeAreaView style={Styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    );
 
-      <View style={Styles.separator} />
+    // <View style={Styles.container}>
+    //   <View>
+    //     <Text style={Styles.title}>
+    //       Featured Daily Rides
+    //     </Text>
+    //   </View>
+
+      /* <View style={Styles.separator} />
 
       <View>
         <Text style={Styles.title}>
@@ -20,18 +54,31 @@ const DailyRidesScreen = ({ navigation }) => {
       </View>
 
       <View style={Styles.separator} />
-      <View>
-        <Text>
-        View on a Map
-        Free Group Rides Map
-        Renton to the Alki Lighthouse
-        Wed, 11/03/2021 - 10:00am | Renton
-        Pace: Steady: [12-14mph] | Distance: 37 miles
-        Register Now!
-        </Text>
-       </View>
+
+      <FlatList
+        style={{
+          flex: 1,
+          // alignSelf: 'flex-start'
+        }}
+        keyExtractor={(item) => item.id}  
+        data={rides}
+        renderItem={({ item }) => (
+          <Text style={Styles.text}>{item.name}</Text>
+        )}
+      />
+        <Card>
+          <Text>View on a Map</Text>
+          <Text>Free Group Rides Map </Text>
+          <Text>Renton to the Alki Lighthouse</Text>  
+          <Text>Wed, 11/03/2021 - 10:00am | Renton</Text>  
+          <Text>Pace: Steady: [12-14mph] | Distance: 37 miles</Text> 
+          <Text>Register Now!</Text>  
+        </Card>
+
+
     </View>
   );
+}; */
 };
 
 export default DailyRidesScreen;
