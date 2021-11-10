0ae603410a1202f4d1334fad71b45faf47b6f595
Alex Bondar <zmagar@gmail.com>
2021-11-10 00:54:35 -0800

    Cleaned the code, used UseSate and UseEffect for fetching data, wrapped all in View to avoid unique key warning

--- components/screens/BlogScreen/BlogScreen.js
+++ components/screens/BlogScreen/BlogScreen.js
@@ -1,3 +1,4 @@
+import { useIsFocused } from '@react-navigation/core';
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {View, Text, ScrollView, Linking, Button} from 'react-native';
@@ -8,43 +9,48 @@ import {Styles} from './BlogStyleSheet';
 
 var blogData = [];
 
-async function getBlogData() {
-  let response = await fetch('https://cascade-api.herokuapp.com/blog');
-  let dataPromise = await response.json();
-  dataPromise.forEach(data => blogData.push(data));
+// async function getBlogData() {
+//   let response = await fetch('https://cascade-api.herokuapp.com/blog');
+//   let dataPromise = await response.json();
+//   dataPromise.forEach(data => blogData.push(data));
 
-  // console.log('OUTSIDE', blogData);
-}
-getBlogData();
-var bData = null;
+//   // console.log('OUTSIDE', blogData);
+// }
+// getBlogData();
 
 
 const BlogScreen = () => {
   
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
+    const isFocused = useIsFocused();
   
     const getData = async () => {
       const resp = await fetch("https://cascade-api.herokuapp.com/blog");
       const data = await resp.json();
       setData(data);
+      console.log(data);
     };
-    // useEffect(() => {
-    //   getData();
-    //   bData = data;
+    useEffect(() => {
+      console.log('Called');
+      if(isFocused) {
+      getData();
+     
+      }
 
-    // }, []);
+    }, []);
  
   return (
     <SafeAreaView>
     <ScrollView>
      
-      {blogData.map(({title, postDate, body, url}) => (
-        <>
+      {data.map(({title, postDate, body, url}) => (
+         <View key = {url}>
           <Text key={title} style={Styles.title}>
             {title}
           </Text>
-          <Text style={Styles.date} key={postDate}>{postDate}</Text>
-          <Image
+          <Text style={Styles.date}
+          key={postDate}>{postDate}</Text>
+          <Image key={'rc34'}
           source={{uri: 'https://cbc-drupal-assets.s3.amazonaws.com/styles/blog_image/s3/blog/leafy%20and%20dank%20%283%29.JPG?R2Pc9TRNWsiPfO0asl_VSPULtFDI4Dqs&itok=O8FLO9W1'}}
           style={Styles.image}
           
@@ -55,7 +61,7 @@ const BlogScreen = () => {
           style={Styles.button}
           type="solid"
           title="Read more"
-          color="#8fa5c9"
+          color="#F55243"
           size={6}
     onPress={(e) => {
       e.preventDefault();
@@ -63,7 +69,7 @@ const BlogScreen = () => {
       }}
 />
 </View>
-        </>
+        </View>
       ))
     }
     </ScrollView>
