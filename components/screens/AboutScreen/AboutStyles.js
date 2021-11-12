9df208a6b911ba3c578cb9f949d92529a73a0230
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-11-12 14:38:35 -0800

    pull in changes

---
+++ components/screens/AboutScreen/AboutStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const Styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginHorizontal: 10
+    },
+    aboutText: {
+        textAlign: 'center'
+    },
+    stayConnectedContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+});
--- components/screens/HomeScreen/HomeScreen.js
+++ components/screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Styles } from './HomeStyleSheet';
+import { Styles } from './HomeStyles';
 import { View, Text } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
