4d240c227f4b7eb0d0a103aa080cfb06c9221c1b
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-11-13 13:46:37 -0800

    added Club logo, url

--- components/screens/HomeScreen/HomeScreen.js
+++ components/screens/HomeScreen/HomeScreen.js
@@ -3,27 +3,20 @@ import { Styles } from './HomeStyles';
 import { View, Text} from 'react-native';
 import {Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Image } from 'react-native-elements';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={Styles.container}>
       <View>
-        <Text style={Styles.title}>
-          Home View
-        </Text>
-      </View>
-
-      <View style={Styles.separator} />
-
-      <View>
+        <Image source={{uri: 'https://cascade.org/sites/all/themes/f1omega/f1ux/logo.png'}}
+       style={{ width: 200, height: 200, resizeMode: 'contain'}} />
         <Text style={Styles.title}>
           Live interactive section of the screen. Rotates between "Join" and "Daily Featured Rides"
         </Text>
-      </View>
 
-      <View style={Styles.separator} />
-    
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      </View>
+      <View style={{ flex: 1, alignItems: 'center'}}>
         <Button
           title=' Daily Featured Rides'
           type='clear'
--- components/screens/HomeScreen/HomeStyles.js
+++ components/screens/HomeScreen/HomeStyles.js
@@ -4,14 +4,22 @@ export const Styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center'
     },
     title: {
         textAlign: 'center',
         marginVertical: 8,
+        color: 'gray'
     },
     separator: {
         marginVertical: 8,
         borderBottomColor: '#737373',
         borderBottomWidth: StyleSheet.hairlineWidth,
+        image: {
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            width: 200,
+            height: 100,
+          }
     },
 });
