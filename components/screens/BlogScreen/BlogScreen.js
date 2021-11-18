537b7fd3d720dda7a347670bc2522488e0e6388e
Alex Bondar <zmagar@gmail.com>
2021-11-17 19:47:24 -0800

    Async fetch function and more

--- components/screens/BlogScreen/BlogScreen.js
+++ components/screens/BlogScreen/BlogScreen.js
@@ -1,62 +1,106 @@
 import { useIsFocused } from '@react-navigation/native';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, ScrollView, Button } from 'react-native';
+import { View, Text, ScrollView, Button, Alert, ActivityIndicator } from 'react-native';
 import { Image } from 'react-native-elements';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Styles } from './BlogStyles';
 
+
+async function openLink(urlToOpen) {
+  try {
+    if (await InAppBrowser.isAvailable()) {
+      const result = await InAppBrowser.open(urlToOpen, {
+        showTitle: false,
+        toolbarColor: '#0176ae',
+        secondaryToolbarColor: 'black',
+        navigationBarColor: 'black',
+        navigationBarDividerColor: 'white',
+        enableUrlBarHiding: true,
+        enableDefaultShare: true,
+        forceCloseOnRedirection: false,
+        // Specify full animation resource identifier(package:anim/name)
+        // or only resource name(in case of animation bundled with app).
+        hasBackButton: true,
+      })
+      console.log(JSON.stringify(result));
+    }
+    else Linking.openURL(urlToOpen)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
 const BlogScreen = () => {
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const isFocused = useIsFocused();
 
   const getData = async () => {
+    setLoading(true);
     const resp = await fetch("https://cascade-api.herokuapp.com/blog");
     const data = await resp.json();
     setData(data);
+    setLoading(false);
+    console.log(data);
   };
   useEffect(() => {
-    if (isFocused) {
-      getData();
+    console.log('useEffect called');
+    getData();
 
-    }
-
-  }, [isFocused]);
+  }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={Styles.container}>
       <ScrollView>
+        {
+          loading ?
+            (
+              
+              <View>
+              <ActivityIndicator
+                animating={true}
+                visible={loading}
+                size="large"
+                color="#0176ae"
+                textContent={'Loading...'} />
+                </View>
+              //   <View>
+              //   <Text>...Loading</Text>
+              // </View>
+            )
 
-        {data.map(({ title, postDate, body, url }) => (
-          <View key={url}>
-            <Text key={title} style={Styles.title}>
-              {title}
-            </Text>
-            <Text style={Styles.date}
-              key={postDate}>{postDate}</Text>
-            <Image key={'rc34'}
-              source={{ uri: 'https://cbc-drupal-assets.s3.amazonaws.com/styles/blog_image/s3/blog/leafy%20and%20dank%20%283%29.JPG?R2Pc9TRNWsiPfO0asl_VSPULtFDI4Dqs&itok=O8FLO9W1' }}
-              style={Styles.image}
+            :
+            data.map(({ title, postDate, body, url, image }) => (
+              <View key={url}>
+                <Text key={title} style={Styles.title}>
+                  {title}
+                </Text>
+                <Text style={Styles.date}
+                  key={postDate}>{postDate}</Text>
+                <Image key={image}
+                  source={{ uri: image }}
+                  style={Styles.image}
 
-            />
-            <Text key={body} style={Styles.blogItem} >{body}</Text>
-            <View key={url} style={{ width: 200, height: 50 }}>
-              <Button
-                style={Styles.button}
-                type="solid"
-                title="Read more"
-                color="#F55243"
-                size={6}
-                onPress={(e) => {
-                  e.preventDefault();
-                  InAppBrowser.open('http://www.cascade.org' + url);
-                }}
-              />
-            </View>
-          </View>
-        ))
+                />
+                <Text key={body} style={Styles.blogItem} >{body}</Text>
+                <View key={url} style={{ width: 200, height: 50 }}>
+                  <Button
+                    style={Styles.button}
+                    type="solid"
+                    title="Read more"
+                    color="#F55243"
+                    size={6}
+                    onPress={(e) => {
+                      e.preventDefault();
+                      openLink('http://www.cascade.org' + url);
+                    }}
+                  />
+                </View>
+              </View>
+            ))
         }
       </ScrollView>
     </SafeAreaView>
