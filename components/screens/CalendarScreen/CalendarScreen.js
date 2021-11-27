d90b76ea7e3b76617a9eebe67e43abf6cb0d0b43
Dagmawi Girma Demissie <dagimethiopia17@gmail.com>
2021-11-26 22:15:28 -0800

    more code cleanup

--- components/screens/CalendarScreen/CalendarScreen.js
+++ components/screens/CalendarScreen/CalendarScreen.js
@@ -3,26 +3,22 @@ import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Styles } from "./CalendarStyles";
 
-//fetch data from the server
 const calendarURL = "https://cascade-api.herokuapp.com/calendar";
 
-//create a calendar screen
 const CalendarScreen = () => {
   const [calendar, setCalendar] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  //fetch data from the server
   useEffect(() => {
     fetch(calendarURL)
       .then((res) => res.json())
       .then((res) => {
         setCalendar(res);
         setIsLoading(false);
-      }) //end of then
+      })
       .catch((err) => console.log(err));
-  }); //end of useEffect
+  });
 
-  //render the calendar screen
   return (
     <SafeAreaView style={Styles.container}>
       <ScrollView>
