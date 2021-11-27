2beb6fe7fdabd3f0372abd75dd2a20c7e96f2480
Dagmawi Girma Demissie <dagimethiopia17@gmail.com>
2021-11-26 22:13:29 -0800

    useEffect hook working well and items rendering correctly.

--- components/screens/CalendarScreen/CalendarScreen.js
+++ components/screens/CalendarScreen/CalendarScreen.js
@@ -1,58 +1,50 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, ScrollView} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Styles } from './CalendarStyle';
+import React, { useState, useEffect } from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Styles } from "./CalendarStyles";
 
 //fetch data from the server
-const calendarURL = 'https://cascade-api.herokuapp.com/calendar';
-
+const calendarURL = "https://cascade-api.herokuapp.com/calendar";
 
 //create a calendar screen
 const CalendarScreen = () => {
-    const [calendar, setCalendar] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const location = ["street", "city", "state", "postal-code", "google-maplink"];
-    //fetch data from the server
-    useEffect(() => {
-        fetch(calendarURL)
-        .then(res => res.json())
-        .then(res => {
-            setCalendar(res);
-            setIsLoading(false);
-        })  //end of then
-        .catch(err => console.log(err));
-    }, []); //end of useEffect
+  const [calendar, setCalendar] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    //render the calendar screen
-    return (
+  //fetch data from the server
+  useEffect(() => {
+    fetch(calendarURL)
+      .then((res) => res.json())
+      .then((res) => {
+        setCalendar(res);
+        setIsLoading(false);
+      }) //end of then
+      .catch((err) => console.log(err));
+  }); //end of useEffect
 
-        <SafeAreaView style={Styles.container}>
-            <ScrollView>
-                {isLoading ? (
-                    <Text>Loading...</Text>
-                ) : ( 
-                    <View>
-                        {calendar.map((item, index) => (
-                            <View key={index} style={Styles.calendarItem}>
-                                <Text style={Styles.date}>{item.date}</Text>
-                                <Text style={Styles.title}>{item.title}</Text>
-                                <Text style={Styles.calendarItem}>{item.description}</Text>
-                                <Text style={Styles.calendarItem}>{item.location}</Text>
-                                <Text style={Styles.calendarItem}>{item.time}</Text>
-                            </View>
-                        ))}
-                       //render location array
-                       <ol>
-                        {location.map((address) => (
-                          <li>{address}</li>
-                        ))}
-                      </ol>
-
-                    </View>
-                )}
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+  //render the calendar screen
+  return (
+    <SafeAreaView style={Styles.container}>
+      <ScrollView>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <View>
+            {calendar.map(({ title, url, date, pace, distance, leader }) => (
+              <View key={url} style={Styles.calendarItem}>
+                <Text style={Styles.title}>{title}</Text>
+                <Text style={Styles.date}>{date}</Text>
+                <Text style={Styles.calendarText}>
+                  {pace + "   " + distance}{" "}
+                </Text>
+                <Text style={Styles.calendarText}>{leader}</Text>
+              </View>
+            ))}
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default CalendarScreen;
