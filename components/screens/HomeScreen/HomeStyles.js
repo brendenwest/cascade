8a2ec7c98b13ae50c42d7d65264f9f61ba4e8635
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-12-12 19:45:59 -0800

    merge rebase

--- components/screens/HomeScreen/HomeStyles.js
+++ components/screens/HomeScreen/HomeStyles.js
@@ -2,52 +2,16 @@ import { StyleSheet } from 'react-native';
 
 export const Styles = StyleSheet.create({
     container: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: "space-evenly",
-        backgroundColor: 'lightGray'
+        flex: 1,
+        justifyContent: 'center',
     },
-    webBtnContainer: {
-        padding: 10
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
     },
-    joinBtn: {
-        backgroundColor: '#f55243',
-        marginBottom: 15,
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    volunteerBtn: {
-        backgroundColor: '#fd9b00',
-        marginBottom: 15,
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    donateBtn: {
-        backgroundColor: '#7b146b',
-        marginBottom: 15,
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    ridesBtnContainer: {
-        alignItems: 'center',
-        marginTop: 30
-    },
-    btnImage: {
-        height: 100,
-        width: 200,
-    },
-    btnText: {
-        color: 'white',
-        fontSize: 25,
-        padding: 10,
-        fontFamily: 'Helvetica',
-        fontWeight: 'bold',
-        textShadowColor: 'black'
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
     },
 });
