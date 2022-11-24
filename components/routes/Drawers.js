904446ea11cb1a6f8a669071cfca673f2899ab40
krishnadave05 <82129655+krishnadave05@users.noreply.github.com>
2022-11-24 11:51:31 -0800

    Minor Github bug fixes

--- components/routes/Drawers.js
+++ components/routes/Drawers.js
@@ -1,80 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { HomeTab } from './Tabs';
-import { Image } from 'react-native';
-import { HeaderBackButton } from '@react-navigation/elements';
-import Icon from 'react-native-vector-icons/AntDesign';
-import AboutScreen from "../screens/AboutScreen/AboutScreen";
-import ContactScreen from "../screens/ContactScreen/ContactScreen";
-import AdvocacyScreen from '../screens/AdvocacyScreen/AdvocacyScreen.js';
-
-
-const Drawer = createDrawerNavigator();
-
-export const DefaultDrawer = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Group screenOptions={({ route }) => ({
-        headerShown: true,
-        drawerIcon: ({ color, size }) => {
-          let iconName;
-          if (route.name === "Home ") {
-            iconName = 'home';
-          } else if (route.name === "About") {
-            iconName = 'questioncircle';
-          } else if (route.name === "Contact Us") {
-            iconName = 'phone';
-          } else if (route.name === "Advocacy") {
-            iconName = 'team';
-          }
-          return <Icon name={iconName} size={size} color={color} />
-        },
-        headerStyle: { backgroundColor: '#0176ae' },
-        headerTintColor: 'white',
-        headerTitleAlign: 'center'
-      })} >
-        <Drawer.Screen name="Home " component={HomeTab}
-          options={{
-            headerTitle: () => (
-              <Image
-                style={{ width: 208, height: 50 }}
-                source={{ uri: 'https://cascade.org/sites/all/themes/f1omega/f1ux/logo.png' }}
-              />
-            )
-          }}
-        />
-        <Drawer.Screen name="About" component={AboutScreen}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <HeaderBackButton
-                tintColor={'white'}
-                onPress={() => navigation.goBack()}
-              />
-            )
-          })} />
-        <Drawer.Screen name="Contact Us" component={ContactScreen}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <HeaderBackButton
-                tintColor={'white'}
-                onPress={() => navigation.goBack()}
-              />
-            )
-          })} />
-        <Drawer.Screen name="Advocacy" component={AdvocacyScreen}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <HeaderBackButton
-                tintColor={'white'}
-                onPress={() => navigation.goBack()}
-              />
-            )
-          })} />
-      </Drawer.Group>
-    </Drawer.Navigator>
-  );
-=======
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomeTab } from './Tabs';
@@ -150,5 +73,4 @@ export const DefaultDrawer = () => {
       </Drawer.Group>
     </Drawer.Navigator>
   );
->>>>>>> Stashed changes
 };
