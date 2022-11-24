805f4dad48da1e78b8931253dcd43a966042aaf9
krishnadave05 <82129655+krishnadave05@users.noreply.github.com>
2022-11-24 11:25:55 -0800

    Bike Classes Added

--- components/routes/Drawers.js
+++ components/routes/Drawers.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomeTab } from './Tabs';
@@ -73,4 +74,81 @@ export const DefaultDrawer = () => {
       </Drawer.Group>
     </Drawer.Navigator>
   );
+=======
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { HomeTab } from './Tabs';
+import { Image } from 'react-native';
+import { HeaderBackButton } from '@react-navigation/elements';
+import Icon from 'react-native-vector-icons/AntDesign';
+import AboutScreen from "../screens/AboutScreen/AboutScreen";
+import ContactScreen from "../screens/ContactScreen/ContactScreen";
+import ClassesScreen from "../screens/ClassesScreen/ClassesScreen";
+
+
+const Drawer = createDrawerNavigator();
+
+export const DefaultDrawer = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Group screenOptions={({ route }) => ({
+        headerShown: true,
+        drawerIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === "Home ") {
+            iconName = 'home';
+          } else if (route.name === "About") {
+            iconName = 'questioncircle';
+          } else if (route.name === "Contact Us") {
+            iconName = 'phone';
+          } else if (route.name === "Classes") {
+          iconName = 'play';
+        }
+          return <Icon name={iconName} size={size} color={color} />
+        },
+        headerStyle: { backgroundColor: '#0176ae' },
+        headerTintColor: 'white',
+        headerTitleAlign: 'center'
+      })} >
+        <Drawer.Screen name="Home " component={HomeTab}
+          options={{
+            headerTitle: () => (
+              <Image
+                style={{ width: 208, height: 50 }}
+                source={{ uri: 'https://cascade.org/sites/all/themes/f1omega/f1ux/logo.png' }}
+              />
+            )
+          }}
+        />
+        <Drawer.Screen name="About" component={AboutScreen}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <HeaderBackButton
+                tintColor={'white'}
+                onPress={() => navigation.goBack()}
+              />
+            )
+          })} />
+        <Drawer.Screen name="Contact Us" component={ContactScreen}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <HeaderBackButton
+                tintColor={'white'}
+                onPress={() => navigation.goBack()}
+              />
+            )
+          })} />
+        <Drawer.Screen name="Classes" component={ClassesScreen}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <HeaderBackButton
+                tintColor={'white'}
+                onPress={() => navigation.goBack()}
+              />
+            )
+          })} />
+      </Drawer.Group>
+    </Drawer.Navigator>
+  );
+>>>>>>> Stashed changes
 };
--- components/routes/Tabs.js
+++ components/routes/Tabs.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -37,4 +38,41 @@ export const HomeTab = () => {
       </Tab.Group>
     </Tab.Navigator >
   );
+=======
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import HomeStack from './Stack';
+import CalendarScreen from '../screens/CalendarScreen/CalendarScreen';
+import BlogScreen from '../screens/BlogScreen/BlogScreen';
+
+const Tab = createBottomTabNavigator();
+
+export const HomeTab = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Group
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Calendar') {
+              iconName = 'calendar';
+            } else if (route.name === 'Blog') {
+              iconName = 'newspaper-o';
+            } 
+            return <Icon name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: '#43b02a',
+          tabBarInactiveTintColor: 'gray',
+        })} >
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="Blog" component={BlogScreen} />
+      </Tab.Group>
+    </Tab.Navigator >
+  );
+>>>>>>> Stashed changes
 };
